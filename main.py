79ba1bdb6141bc60624498fbb80f82aae0e34d11
Георгій <georgiyman07@gmail.com>
2025-04-28 21:47:29 +0300

    новая бд

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from contextlib import asynccontextmanager
 # ======================== КОНФИГ ========================
 TELEGRAM_TOKEN = "7100808154:AAGGeC57S4T-_eXqpXowI-ZB-vv-ltqjSmY"
 ROUTER_API_KEY = "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"
-DB_URL = "postgresql+asyncpg://georgiy:hk0sqGETCp5kyOKmB2nkOZ8kRo5wNEYN@dpg-d060qa2li9vc73br3fj0-a/virtbot"
+DB_URL = "postgresql+asyncpg://georgiy:19A0DjfgzwFazftwluXKKFaZU60jeY3T@dpg-d07spb2dbo4c73bs7l4g-a/virtbot"
 NOWPAYMENTS_API_KEY = "BVVS1E5-A2X4H0X-N9S5YTD-EA9MVRS"
 NOWPAYMENTS_API_URL = "https://api.nowpayments.io/v1/invoice"
 WEBHOOK_URL = "https://bottg-sgjr.onrender.com/webhook"
